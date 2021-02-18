b79c9b812e0f53b794005d65a0305d1fa06c5781
Juel B <juel.braganza97@gmail.com>
2021-02-18 02:07:10 +0000

    pass tests for Booleans functions

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,56 @@
-const negate = a => {
-  // your code here
-};
+const negate = a => !a;
 
-const both = (a, b) => {
-  // your code here
-};
+const both = (a, b) => a && b;
 
-const either = (a, b) => {
-  // your code here
-};
+const either = (a, b) => a || b;
 
-const none = (a, b) => {
-  // your code here
-};
+const none = (a, b) => !a && !b;
 
-const one = (a, b) => {
-  // your code here
-};
+const one = (a, b) => (!a && b) || (a && !b);
 
 const truthiness = a => {
-  // your code here
+  if (a) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const isEqual = (a, b) => {
-  // your code here
-};
+const isEqual = (a, b) => a === b;
 
-const isGreaterThan = (a, b) => {
-  // your code here
-};
+const isGreaterThan = (a, b) => a > b;
 
-const isLessThanOrEqualTo = (a, b) => {
-  // your code here
-};
+const isLessThanOrEqualTo = (a, b) => a <= b;
 
-const isOdd = a => {
-  // your code here
-};
+const isOdd = a => a % 2 !== 0;
 
-const isEven = a => {
-  // your code here
-};
+const isEven = a => a % 2 === 0;
 
 const isSquare = a => {
-  // your code here
-};
+  const sqr = Math.sqrt(a);
+  return sqr * sqr === a;
+}
 
 const startsWith = (char, string) => {
-  // your code here
+  return string[0] === char;
 };
 
 const containsVowels = string => {
-  // your code here
+  const lowercaseStr = string.toLowerCase();
+  return (
+    lowercaseStr.includes("a") ||
+    lowercaseStr.includes("e") ||
+    lowercaseStr.includes("i") ||
+    lowercaseStr.includes("o") ||
+    lowercaseStr.includes("u")
+  );
 };
 
 const isLowerCase = string => {
-  // your code here
+  for (let i = 0; i < string.length; i += 1) {
+    return string[i] === string[i].toLowerCase();
+  }
+  return false;
 };
 
 module.exports = {
